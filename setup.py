859c0dd4255a26d17c6bd5d31eec4ea9a23b7351
Sepehr Saryazdi <sepehr.saryazdi@gmail.com>
2025-01-09 11:22:34 +1100

    Added src and init file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='trace_length_reduction',
+   version='1.0',
+   description='Trace Length Reduction Package',
+   license="MIT",
+   long_description=long_description,
+   author='Sepehr Saryazdi',
+   author_email='sepehr.saryazdi@sydney.edu.au',
+   url="https://github.com/sepehrsaryazdi/trace_length_reduction",
+   packages=find_packages(exclude=['tests']),  #same as name
+   install_requires=['wheel', 'numpy', 'sympy', 'matplotlib', 'pandas'], #external packages as dependencies
+)
