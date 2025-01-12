6856ab0635581e8af08949f61fa824f19c5e4fae
Sepehr Saryazdi <sepehr.saryazdi@gmail.com>
2025-01-12 12:56:02 +1100

    Added main algorithm interfaces

--- trace_length_reduction/reduction.py
+++ trace_length_reduction/reduction.py
@@ -61,6 +61,220 @@ def trmax(x):
     return (sp.exp(x)+2)*sp.exp(-x/sp.Number(3))
 
 
+def Y_k(alpha,beta,k):
+    return (beta, (alpha**(-1))*(beta**k))
+
+def X_k(alpha,beta,k):
+    return (beta, alpha*(beta**k))
+
+
+def diagonalise_in_order(B, precision=100):
+    """
+    Assumes B hyperbolic and diagonalises B such that B = PDP^(-1) and D = diag(l1,l2,l3), l1 > l2 > l3.
+    """
+    ls, vs = [],[]
+    eigenvectors = B.eigenvects()
+    assert len(eigenvectors) == 3, "B does not have 3 distinct eigenvectors"
+    for eigenvector in B.eigenvects():
+        ls.append(eigenvector[0])
+        vs.append(eigenvector[2][0])
+    ls_precision = [N(ls[i],precision).as_real_imag()[0] for i in range(3)]
+    argsort = np.argsort(ls_precision)[::-1] # sorts eigenvalues such that first is largest
+    ls = np.array(ls)[argsort]
+    vs = np.array(vs)[argsort]
+    D = sp.Matrix([[ls[0],0,0],[0,ls[1],0],[0,0,ls[2]]])
+    P = sp.Matrix(np.hstack([*vs]))
+    P,D = N(P,precision).as_real_imag()[0], N(D,precision).as_real_imag()[0] # converts to real matrix
+    return (P,D)
+
+
+def get_k0_k1(A,B, s, move):
+
+    P,D = diagonalise_in_order(B)
+
+    if move == Y_k:
+        A = A.inv()
+
+    A_tilde = P.inv()*A*P
+
+    a,b,c = A_tilde[0,0], A_tilde[1,1], A_tilde[2,2]
+    
+    l1,l2,l3 = D[0,0], D[1,1], D[2,2]
+
+    if b!=0:
+        k1 = int(sp.ceiling(sp.Max(0,
+            sp.log(a/2/(2*sp.Abs(b)))/sp.log(l2/l1),
+            sp.log(a/2/(2*c))/sp.log(l3/l1),
+            sp.log(s/(a/2))/sp.log(l1))))
+        k0 = -int(sp.ceiling(sp.Max(0,
+            sp.log(c/2/(2*sp.Abs(b)))/sp.log(l3/l2),
+            sp.log(c/2/(2*a))/sp.log(l3/l1),
+            sp.log(s/(c/2))/sp.log(1/l3))))
+    
+    else:
+        k1 = int(sp.ceiling(sp.Max(0,
+            sp.log(a/2/c)/sp.log(l3/l1),
+            sp.log(s/(a/2))/sp.log(l1))
+        ))
+        k0 = -int(sp.ceiling(sp.Max(0,
+            sp.log(c/2/a)/sp.log(l3/l1),
+            sp.log(s/(c/2))/sp.log(1/l3))
+        ))
+
+    return [k0,k1]
+
+
+
+def get_bounds(alpha,beta,objective,move, delta=0):
+    """
+    Constructs bounds [k0,k1] such that the complement [k0,k1] necessarily guarantees the values are negative. If no values exist, it returns an empty array. Assumes the value of trace is strictly concave up and has a global minimiser.
+    """
+    objective_function, objective_label = objective
+
+
+    if objective_label == 'trace':
+        s = sp.trace(beta) + delta
+    else:
+        s = trmax(objective_function(beta) + delta) # lmin^(-1)(l(beta))
+
+
+    k0,k1 = get_k0_k1(alpha,beta,s,move)
+
+    def f(k):
+        return s - sp.trace(move(alpha,beta,k)[1])
+    
+    return [k0, k1]
+
+def main_algorithm(alpha, beta, objective, visited_generators=[],expression=(sp.Symbol('a',commutative=False),sp.Symbol('b',commutative=False)), verbose=False):
+    assert isinstance(alpha, sp.Matrix), "Error: alpha is not a sp.Matrix"
+    assert isinstance(beta, sp.Matrix), "Error: beta is not a sp.Matrix"
+
+
+    objective_function, objective_label = objective
+
+    if objective_function(alpha) < objective_function(beta):
+        alpha,beta = beta,alpha
+
+        
+    def objective_difference(A,B):
+        return objective_function(A) - objective_function(B)
+
+    def f(k):
+        return objective_difference(*X_k(alpha,beta,k))
+
+    def g(k):
+        return objective_difference(*Y_k(alpha,beta,k))
+
+    visited_generators.append((alpha,beta))
+    
+    candidate_moves = [] # elements are tuples with powers, then X_k or Y_k
+    
+    bounds = get_bounds(alpha,beta, objective, X_k)
+
+    if bounds:
+        [k0_algorithm,k1_algorithm] = bounds
+        k_vals = np.linspace(k0_algorithm, k1_algorithm, k1_algorithm-k0_algorithm+1, dtype=np.int64)
+
+        positive_k_vals = []
+        for k in k_vals:
+            if f(k)>0:
+                positive_k_vals.append(k)
+
+        if len(positive_k_vals):
+            for k in positive_k_vals:
+                candidate_moves.append((k, X_k, f(k)))
+    
+    bounds = get_bounds(alpha,beta, objective, Y_k)
+
+    if bounds:
+        [k0_algorithm,k1_algorithm] = bounds
+        k_vals = np.linspace(k0_algorithm, k1_algorithm, k1_algorithm-k0_algorithm+1, dtype=np.int64)
+        
+        positive_k_vals = []
+        for k in k_vals:
+            if g(k)>0:
+                positive_k_vals.append(k)
+
+        if len(positive_k_vals):
+            for k in positive_k_vals:
+                candidate_moves.append((k, Y_k, g(k)))
+
+    if verbose:   
+        print(candidate_moves, 'candidate moves', [float(candidate_moves[i][-1]) for i in range(len(candidate_moves))])
+
+    
+    if candidate_moves:
+        values = [candidate_moves[i][2] for i in range(len(candidate_moves))]
+        best_move = candidate_moves[np.argmax(values)]
+        k = best_move[0]
+        move_function = best_move[1]
+
+        expression = move_function(expression[0],expression[1], k)
+
+        if verbose:
+            print("algorithm move applied:", f"{str(move_function).rsplit(" ")[1]} {k}", expression)
+
+
+        alpha_prime, beta_prime, visited_generators,expression = main_algorithm(*move_function(alpha,beta, k), objective,visited_generators, expression)
+
+    else:
+        alpha_prime,beta_prime = alpha,beta
+        if verbose:
+            print('no candidate moves')
+
+
+    #### post processing
+
+
+    def f(k):
+        return objective_difference(*X_k(alpha_prime, beta_prime,k)) + objective_difference(alpha_prime,beta_prime)
+    
+    def g(k):
+        return objective_difference(*Y_k(alpha_prime, beta_prime,k)) + objective_difference(alpha_prime,beta_prime)
+
+
+    candidate_moves = [] # elements are tuples with powers, then X_k or Y_k
+    
+    bounds = get_bounds(alpha_prime,beta_prime,objective, X_k, delta=objective_difference(alpha_prime,beta_prime))
+
+    if bounds:
+        [k0_algorithm,k1_algorithm] = bounds
+        k_vals =  np.linspace(k0_algorithm, k1_algorithm, k1_algorithm-k0_algorithm+1, dtype=np.int64)
+        if len(k_vals):
+            for k in k_vals:
+                candidate_moves.append((k, X_k, f(k)))
+
+    bounds = get_bounds(alpha_prime,beta_prime,objective,Y_k,delta=objective_difference(alpha_prime,beta_prime))
+    if bounds:
+        [k0_algorithm,k1_algorithm] = bounds
+        k_vals = np.linspace(k0_algorithm, k1_algorithm, k1_algorithm-k0_algorithm+1, dtype=np.int64)
+        if len(k_vals):
+            for k in k_vals:
+                candidate_moves.append((k, Y_k, g(k)))
+
+    if verbose:        
+        print(candidate_moves, 'post processing candidate moves', [float(candidate_moves[i][-1]) for i in range(len(candidate_moves))])
+
+    
+    if candidate_moves:
+        values = [candidate_moves[i][2] for i in range(len(candidate_moves))]
+        best_move = candidate_moves[np.argmax(values)]
+        k = best_move[0]
+        move_function = best_move[1]
+
+        expression = X_k(*move_function(expression[0],expression[1], k),0)
+
+        if verbose:
+            print("post processing algorithm move applied:", f"{str(move_function).rsplit(" ")[1]} {k}", expression)
+
+        alpha_prime, beta_prime = X_k(*move_function(alpha_prime,beta_prime, k),0)
+
+    else:
+        if verbose:
+            print('no candidate post processing moves')
+
+    return alpha_prime, beta_prime, visited_generators, expression
+
 
 class XCoords():
     def __init__(self, coords, cube_roots=[]):
@@ -72,8 +286,8 @@ class XCoords():
         
         assert len(coords) == 8, f"Error: {coords} must contain 8 coordinates. It has {len(coords)}."
         assert len(cube_roots) == 8, f"Error: {cube_roots} must contain 8 coordinates. It has {len(cube_roots)}."
-        assert np.all([(isinstance(coords[i], sp.Number) or isinstance(coords[i], sp.core.power.Pow)) for i in range(len(coords))]), f"Error: {coords} must be instances of sympy numbers or powers. They are {[coords[i].__class__ for i in range(len(coords))]}"
-        assert np.all([(isinstance(cube_roots[i], sp.Number) or isinstance(cube_roots[i], sp.core.power.Pow)) for i in range(len(cube_roots))]), f"Error: {cube_roots} must be instances of sympy numbers or powers. They are {[cube_roots[i].__class__ for i in range(len(cube_roots))]}"
+        assert np.all([isinstance(coords[i], sp.Expr) for i in range(len(coords))]), f"Error: {coords} must be instances of sympy numbers or sympy expressions. They are {[coords[i].__class__ for i in range(len(coords))]}"
+        assert np.all([isinstance(cube_roots[i], sp.Expr) for i in range(len(cube_roots))]), f"Error: {cube_roots} must be instances of sympy numbers or powers. They are {[cube_roots[i].__class__ for i in range(len(cube_roots))]}"
         assert np.all([sp.Pow(cube_roots[i],3) for i in range(len(cube_roots))] == coords), "Error: cube roots are not cube roots of coords."
 
         self.coords = coords
@@ -82,15 +296,43 @@ class XCoords():
     def get_coords(self):
         return (self.coords, self.cube_roots)
 
-
 class ReductionResults():
     def __init__(self):
         pass
 
-class TraceLengthObj():
+class Objective():
+    def __init__(self, objective_label):
+        assert objective_label == 'trace' or objective_label == 'length', f"Error: objective_label {objective_label} is not 'trace' or 'length'."
+        self.objective_label = objective_label
+        if self.objective_label == 'trace':
+            self.objective_function = sp.trace 
+        else:
+            self.objective_function = calculate_geodesic_length
+
+    def get_objective(self):
+        return (self.objective_function, self.objective_label)
+        
+class TraceLengthReductionInterface():
     def __init__(self, x):
         assert isinstance(x, XCoords), "Error: x must be an instance of XCoords."
         coords, cube_roots = x.get_coords()
         self.generators = compute_translation_matrix_torus(*coords, *cube_roots)
+    
+    def trace_reduction(self, verbose=False):
+        alpha_returned, beta_returned, visited_generators_trace, expression = main_algorithm(*self.generators, Objective('trace').get_objective(), verbose=verbose)
+        # print(visited_generators_trace)
 
-TraceLengthObj(XCoords([sp.Number(1)]*8))
+    def length_reduction(self, verbose=False):
+        alpha_returned, beta_returned, visited_generators_length, expression = main_algorithm(*self.generators, Objective('length').get_objective(), verbose=verbose)
+
+
+# random_integers = [1,1]*2 +[15, 10]*6 # special end longer
+# random_rationals = [sp.Number(random_integers[2*i])/sp.Number(random_integers[2*i+1]) for i in range(8)]
+# random_rationals[2]=sp.Number(1)/random_rationals[3]
+# random_rationals[4]=sp.Number(1)/random_rationals[5]
+# random_rationals[6]=sp.Number(1)/random_rationals[7]
+# random_rationals[1] = sp.Number(1)/(sp.Number(2)*random_rationals[0])
+
+# # TraceLengthReductionInterface(XCoords(random_rationals)).trace_reduction()
+
+# TraceLengthReductionInterface(XCoords([sp.Number(1)]*8)).trace_reduction()
